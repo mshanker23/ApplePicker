30d0ce29f0c93605f9b23bfd78794e365f318da6
Meghna Shanker <mshanker823@gmail.com>
2019-02-06 19:35:44 -0500

    Made AppleTree Move and Drop Apples
    Edited AppleTree script so that AppleTree gameobject moved across screen and changes direction randomly while dropping an Apple every second. Also created and assigned physics layers to prevent Apples from colliding with each other and AppleTree.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set In Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
+
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
     }
 
-    // Update is called once per frame
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);      
+        apple.transform.position = transform.position;                  
+        Invoke("DropApple", secondsBetweenAppleDrops);
+
+    }
+
+
     void Update()
     {
-        
+
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+
+
+
+
+
+
     }
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+
+
 }
